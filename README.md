e3d4ea5a23a92a4204f45e2b4550c8b610b36e80
Pedro Antunes <antunespedromh@gmail.com>
2023-03-07 16:21:49 -0300

    dsdsa

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Intro-Algorithms
 Code implementation of algorithms studied in DAS410047- Introdução a Algoritmos
+
+Author: Pedro Marcolin Antunes - Federal University of Santa Catarina - Brazil
--- merge_sort.py
+++ merge_sort.py
@@ -5,30 +5,38 @@ import math
 A = [7, 4, 6, 10, 2, 5, 3, 13, 7, 9, 10, 50, 2, 3, 6]
 
 def merge_sort(A,s,e):
-    if s < e:
-        m = (s+e) // 2
-        merge_sort(A,s,m)
-        merge_sort(A,m+1,e)
-        merge(A,s,m,e)
-    else: 
+    if e-s <= 1:
         return A[s:e+1]
+    m = (s+e) // 2
+    merge_sort(A,s,m)
+    merge_sort(A,m,e)
+    merge(A,s,m,e)
+    return A
+
 
 def merge(A,s,m,e):
     left = A[s:m]
-    right = A[m+1:e+1]
+    right = A[m:e]
     i = 0
     j = 0
-    k = 0
     B = []
-    while i != len(left) or j != len(right):
-        if left[i] >= right[j]:
-            B[k] = left[i]
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            B.append(left[i])
             i = i + 1
-            k = k + 1
         else:
-            B[k] = right[j]
+            B.append(right[j])
             j = j + 1
-            k = k + 1
+    
+    while i < len(left):
+        B.append(left[i])
+        i = i + 1
+
+    while j < len(right):
+        B.append(right[j])
+        j = j + 1
     return B
 
-merge_sort(A,0,15)
+B = merge_sort(A,0,15)
+print(B)
