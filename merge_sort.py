2a55135a4682f0d997548e0ca5482b6766b24fde
Pedro Antunes <antunespedromh@gmail.com>
2023-03-08 14:52:12 -0300

    Merge sort finished

--- merge_sort.py
+++ merge_sort.py
@@ -2,27 +2,27 @@ import numpy as np
 import pytest
 import math  
 
-A = [7, 4, 6, 10, 2, 5, 3, 13, 7, 9, 10, 50, 2, 3, 6]
+A = [7, 4, 6, 10, 2, 5, 3, 13, 7, 9, 10, 50, 2, 3, 6 ,1, 2,3,7,5,8,0,2,2,2,2,5,5,7,8,9,10]
 
-def merge_sort(A,s,e):
-    if e-s <= 1:
-        return A[s:e+1]
-    m = (s+e) // 2
-    merge_sort(A,s,m)
-    merge_sort(A,m,e)
-    merge(A,s,m,e)
-    return A
+def merge_sort(A):
+    size = len(A)
+    if size == 1:
+        return A
+    else:
+        m = size // 2
+        left = A[:m]
+        right = A[m:]
+        left = merge_sort(left)
+        right = merge_sort(right)
+        return merge(left,right)
 
-
-def merge(A,s,m,e):
-    left = A[s:m]
-    right = A[m:e]
+def merge(left, right):
     i = 0
     j = 0
     B = []
 
     while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
+        if left[i] < right[j]:
             B.append(left[i])
             i = i + 1
         else:
@@ -38,5 +38,5 @@ def merge(A,s,m,e):
         j = j + 1
     return B
 
-B = merge_sort(A,0,15)
+B = merge_sort(A)
 print(B)
